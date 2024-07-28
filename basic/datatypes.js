2a312abe1e82b9f292cd96f15623b1dc666ced42
Jk <128121767+MUHAMMMADJUNAID1@users.noreply.github.com>
2024-07-28 17:49:07 +0000

    working on the user mindsets

---
+++ basic/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+// we are using node js not brawser so alert not work
+
+let name = "Junaid"
+let age = 22
+let isLoggedIn = true
+
+// null standlone value  type   undefiend  representation of empty value
+// undefined means the value not assigned
+
+//symbol  finding the uniqueness  => 
+
+
+//object
+
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,27 @@
-console.log("hello github");
+const accountId = 133444;  // const can not be changed 
+
+let accountEmail = "jk066726@gmail.com"
+
+var accountPassword = "12345"
+ let accountState;
+
+
+/*
+    prefer not to use var beacuse of an issue  in block and functional scope
+*/
+
+accountCity = "jaipur"
+
+//accountId = 2;  // Error: Cannot assign a new value to a constant
+
+console.log(accountId)
+
+accountEmail = "john.doe@example.com";
+
+
+
+accountPassword = "password123";
+ 
+accountCity = "delhi";
+
+console.table([accountId, accountEmail, accountPassword,  accountState, accountCity])
